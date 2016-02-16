29966321f8fd433b5a229d9e62121251b0df12fe
busayo <thrustinv@gmail.com>
2016-02-16 19:03:04 +0100

    Added Cron.php

---
+++ cron.php
@@ -0,0 +1,28 @@
+<?php
+
+$platforms = array(
+    "freesia1" => "8585",
+    "freesia2" => "8585",
+    "mobility" => "8585",
+    "starfish" => "8585"
+);
+
+//Counter for number of processes
+$i = 1;
+
+foreach($platforms as $key => $value) {
+    $pid = pcntl_fork();
+    if ( ! $pid) {
+        echo 'starting child ', $i, PHP_EOL;
+        $statusController = new \Controller\StatusController($key);
+        file_put_contents("$key.json", json_encode($statusController->resolveModule("sms_c")));
+        exit();
+    }
+    $i++;
+}
+
+//Wait for all the subprocesses to complete to avoid zombie processes
+foreach($platforms as $key)
+{
+    pcntl_wait($status);
+}
